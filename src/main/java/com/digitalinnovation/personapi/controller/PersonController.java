1d7035607a15595903a3ec6d88bd9055b7f73f95
LucasLosano <losano3@hotmail.com>
2021-06-17 16:06:39 -0300

    Repairs

--- src/main/java/com/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/com/digitalinnovation/personapi/controller/PersonController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
@@ -15,6 +16,11 @@ public class PersonController {
     @Autowired
     public PersonController(PersonService personService){
         this.personService = personService;
+    }
+
+    @GetMapping
+    public String getResponse(){
+        return "Hello World";
     }
 
     @PostMapping
